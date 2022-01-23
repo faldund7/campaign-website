9291b41e42a72ecb0b430bbe74d82e7c019a0d4b
Zenith Zack <faldund@outlook.com>
2022-01-23 17:51:26 -0500

    added layout to HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './components/HomeScreen.js'
 
 function App() {
   return (
-    <div>
+    <div className='app'>
+      <HomeScreen/>
     </div>
   );
 }
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomeScreen() {
+  return (
+        <div className='homeScreen'>
+            {/* Nav */}
+            {/* Banner */}
+            {/* Row */}
+        </div>
+    );
+}
+
+export default HomeScreen;
