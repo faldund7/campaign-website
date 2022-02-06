cc68dcf65d79a0270b1396fb886d1ef386361e9e
Zenith Zack <faldund@outlook.com>
2022-02-06 12:21:33 -0500

    removed most errors from modal logic

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,77 @@
 import React from 'react';
 import { motion, AnimatePresence } from "framer-motion";
+import { modalOverlayVariants, staggerOne, modalFadeInUpVariants,  modalVariants} from './motionUtils.js';
+import './Modal.scss';
+import { FaMinus, FaPlay, FaPlus } from "react-icons/fa";
+import { VscChromeClose } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+
 
 function Modal() {
     return(
         <div>
-            <AnimatePresence exitbeforeEnter>
-                <>
-                    <motion.div
-                        variants={modalOverlayVariants}
+            
+		<AnimatePresence exitBeforeEnter>
+			{!modalClosed && (
+				<>
+					<motion.div
+						variants={modalOverlayVariants}
 						initial="hidden"
 						animate="visible"
 						exit="hidden"
 						key="modalOverlay"
-						className={`Modal__overlay ${modalClosed && 'Modal__invisible'}`}>
-
-                    </motion.div>
-                </>
-            </AnimatePresence>
+						className={`Modal__overlay ${modalClosed && 'Modal__invisible'}`}
+					>
+						<motion.div
+							key="modal"
+							variants={modalVariants}
+							ref={modalRef}
+							className={`Modal__wrp ${modalClosed && 'Modal__invisible'}`}
+						>
+							<motion.button
+								className="Modal__closebtn"
+								onClick={handleModalClose}
+							>
+								<VscChromeClose />
+							</motion.button>
+							<div className="Modal__image--wrp">
+								<div className="Modal__image--shadow" />
+								<img
+									className="Modal__image--img"
+									src="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+									alt="Some Random"
+								/>
+								<div className="Modal__image--buttonswrp">
+									<Link
+										className="Modal__image--button"
+										// onClick={handlePlayAnimation}
+										// to={'/play'}
+									>
+										<FaPlay />
+										<span>Play</span>
+									</Link>
+									{/* {!isFavourite
+										? (
+											<button className='Modal__image--button-circular' onClick={handleAdd}>
+												<FaPlus />
+											</button>
+										): (
+											<button className='Modal__image--button-circular' onClick={handleRemove}>
+												<FaMinus />
+											</button>
+										)} */}
+								</div>
+							</div>
+							<motion.div variants={staggerOne} initial="initial" animate="animate" exit="exit" className="Modal__info--wrp">
+								<motion.h3 variants={modalFadeInUpVariants} className="Modal__info--title">Dhathrutv Baddam Reddy</motion.h3>
+								<motion.p variants={modalFadeInUpVariants} className="Modal__info--description">Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.</motion.p>
+								<motion.hr variants={modalFadeInUpVariants} className="Modal__info--line"/>
+							</motion.div>
+						</motion.div>
+					</motion.div>
+				</>
+			)}
+		</AnimatePresence>
         </div>
     ); 
 }
--- src/components/motionUtils.js
+++ src/components/motionUtils.js
@@ -2,3 +2,17 @@ export const modalOverlayVariants = {
 	hidden: { opacity: 0, transition: { duration: .2, delay: .2 } },
 	visible: { opacity: 1, transition: { duration: .2 } }
 }
+
+export const staggerOne = {
+	animate: { transition: { staggerChildren: .1 } }
+}
+
+export const modalFadeInUpVariants = {
+	initial: { y: 60, opacity: 0, transition: { duration: .8, ease: defaultEasing } },
+	animate: { y: 0, opacity: 1, transition: { duration: .8, ease: defaultEasing } }
+};
+
+export const modalVariants = {
+	hidden: { opacity: 0, top: "100%", transition: { duration: .8, ease: defaultEasing } },
+	visible: { opacity: 1, top: "50%", transition: { duration: .8, ease: defaultEasing } }
+}
