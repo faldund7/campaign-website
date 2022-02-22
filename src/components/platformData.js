8d20e5321a27859f2099316a908445cf8fdc803d
Zenith Zack <faldund@outlook.com>
2022-02-21 22:52:54 -0500

    qa_platform png and video

--- src/components/platformData.js
+++ src/components/platformData.js
@@ -64,6 +64,15 @@ const platformData = [
     },
     {
         id: 11,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/qa_platform.png',
+        video_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/qa_platform.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/qa_platform.png'
+    },
+    {
+        id: 12,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/clifton_map.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
