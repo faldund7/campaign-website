55ec39a6c548bf0753777134dc2f971c58fa46d6
Zenith Zack <faldund@outlook.com>
2022-02-21 22:13:00 -0500

    halal foods

--- src/components/platformData.js
+++ src/components/platformData.js
@@ -10,6 +10,24 @@ const platformData = [
     },
     {
         id: 6,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/uc_fest.png',
+        video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/uc_fest.png'
+    },
+    {
+        id: 7,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/halal_food.png',
+        video_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/halal_food.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/halal_food.png'
+    },
+    {
+        id: 8,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/clifton_map.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
@@ -17,6 +35,7 @@ const platformData = [
         modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/clifton_map.png'
     }
+
 ]
 
 export default platformData;
