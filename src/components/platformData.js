dd51752825799c3cbbddd32e68d2875ab822f15b
Zenith Zack <faldund@outlook.com>
2022-02-21 22:20:12 -0500

    ipads for all png and video

--- src/components/platformData.js
+++ src/components/platformData.js
@@ -28,6 +28,15 @@ const platformData = [
     },
     {
         id: 8,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/ipads_for_all.png',
+        video_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/ipads_for_all.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/ipads_for_all.png'
+    },
+    {
+        id: 9,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/clifton_map.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
