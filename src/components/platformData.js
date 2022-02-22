342d3de2d83ba415ddc1c8f1b66eebd3e6361896
Zenith Zack <faldund@outlook.com>
2022-02-21 22:36:45 -0500

    orientation png added

--- src/components/platformData.js
+++ src/components/platformData.js
@@ -45,7 +45,25 @@ const platformData = [
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/free_cvg_shuttle.png'
     },
     {
+        id: 9,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/syllabus_bank.png',
+        video_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/syllabus_bank.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/syllabus_bank.png'
+    },
+    {
         id: 10,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/orientation.png',
+        video_url: '',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/orientation.png'
+    },
+    {
+        id: 11,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/clifton_map.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
