13d544f8f0b4c5b7c7c43f037b815d3ec23b385a
Zenith Zack <faldund@outlook.com>
2022-02-28 00:52:48 -0500

    added what-we-stand-for pictures

--- src/components/whatWeStandForData.js
+++ src/components/whatWeStandForData.js
@@ -1,21 +1,30 @@
 const whatWeStandForData = [
     {
         id: 3,
-        url: 'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/youthful.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Dhathrutv Arya Reddy',
         modalTitle: 'Hello, World!',
         modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
-        thumbnail_url: 'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg'
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/youthful.png'
     },
     {
         id: 4,
-        url: 'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/opportunity.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
         modalTitle: 'Hello, World!',
         modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
-        thumbnail_url: 'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg'
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/opportunity.png'
+    }, 
+    {
+        id: 4,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/unity.png',
+        video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
+        name: 'Lukos',
+        modalTitle: 'Hello, World!',
+        modalInfo: 'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/unity.png'
     }
 ]
 
