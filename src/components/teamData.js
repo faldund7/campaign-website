dcf829d74d82538741e0c4c5449068ca8536c426
Zenith Zack <faldund@outlook.com>
2022-02-28 04:11:53 -0500

    added a team member picture

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -4,10 +4,11 @@ const teamData = [
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/preet.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Dhathrutv Arya Reddy',
-        modalTitle: 'Hello, World!',
+        modalTitle: 'Prit Khimasia',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/preet.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 8,
@@ -17,177 +18,206 @@ const teamData = [
         modalTitle: 'Nishil Faldu',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/nishil.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 9,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/anusha.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Anusha Chitranshi',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/anusha.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 10,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/caitlyn.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Caitlyn Gentry',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/caitlyn.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 11,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/carolyn.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Caroline Sanders',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/carolyn.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 12,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/diya.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Diya Joshi',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/diya.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 13,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/gaby.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Gabrielle Scott',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/gaby.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 14,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/hussain.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/abhigyan.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Abhigyan Acherjee',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/hussain.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/abhigyan.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 15,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/kiara.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/hussain.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Husain Bohara',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/kiara.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/hussain.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 16,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/mihir.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/kiara.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Ka’Yare Dickson',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/mihir.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/kiara.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 17,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/natik.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/mihir.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Mihir Mahale',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/natik.png',
-        what_why_flag: false
-    },
-    {
-        id: 8,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/pallavi.png',
-        video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
-        name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
-        modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/pallavi.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/mihir.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 18,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/priti.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/natik.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Natiq Fazlani',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/priti.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/natik.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 19,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/rahul.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/pallavi.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Pallavi Katragadda',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/rahul.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/pallavi.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 20,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/renato.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/priti.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Priti Vitankar',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/renato.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/priti.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 21,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/terry.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/rahul.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Rahul Shakya',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/terry.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/rahul.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 22,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tom.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/renato.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Renato',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tom.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/renato.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 23,
-        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tyra.png',
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/terry.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Terri Andreoni-Armstrong',
         modalInfo: '',
-        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tyra.png',
-        what_why_flag: false
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/terry.png',
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 24,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tom.png',
+        video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
+        name: 'Lukos',
+        modalTitle: 'Tom Feldman',
+        modalInfo: '',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tom.png',
+        what_why_flag: false,
+        video_flag: false
+    },
+    {
+        id: 25,
+        url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tyra.png',
+        video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
+        name: 'Lukos',
+        modalTitle: 'Tira Hawkins',
+        modalInfo: '',
+        thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/tyra.png',
+        what_why_flag: false,
+        video_flag: false
+    },
+    {
+        id: 26,
         url: 'https://2022campaign.s3.us-east-2.amazonaws.com/yagna.png',
         video_url: 'https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4',
         name: 'Lukos',
-        modalTitle: 'NISHIL FALDU',
+        modalTitle: 'Yagna Shah',
         modalInfo: '',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/yagna.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     }
 ]
 
--- src/components/whatWeStandForData.js
+++ src/components/whatWeStandForData.js
@@ -7,7 +7,8 @@ const whatWeStandForData = [
         modalTitle: 'Youthful',
         modalInfo: 'We want to step out of our comfort zone, attempt the impossible and put our heart and soul into making it happen, proving that youthfulness is not about an age, it\'s about a spirit, and us Bearcats never fall short of this spirit.',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/youthful.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     },
     {
         id: 4,
@@ -17,7 +18,8 @@ const whatWeStandForData = [
         modalTitle: 'Opportunity',
         modalInfo: '‘You Only Live Once’, before their time in college runs out, we want every Bearcat to make the most of their college life with opportunities to grow academically, professionally, artistically, personally, and in every other positive direction you can imagine.',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/opportunity.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     }, 
     {
         id: 4,
@@ -27,7 +29,8 @@ const whatWeStandForData = [
         modalTitle: 'Unity',
         modalInfo: 'To bring together all the clubs, organizations, colleges and the students at UC and make them work towards one UC. ',
         thumbnail_url: 'https://2022campaign.s3.us-east-2.amazonaws.com/unity.png',
-        what_why_flag: false
+        what_why_flag: false,
+        video_flag: false
     }
 ]
 
